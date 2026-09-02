e293a3ee78ad61974da4e76f161c4c295914692a
aghony <kerenjustin286@gmailcom>
2026-09-02 13:07:34 +0700

    finish weapon cpp and updated filed

--- Studi-Kasus/Weapon.cpp
+++ Studi-Kasus/Weapon.cpp
@@ -1,3 +1,8 @@
+// Question 1
+//A. Buatlah kelas Weapon dengan atribut name (nama senjata) dan damage (kerusakan senjata).
+//B. Implementasikan konstruktor untuk menginisialisasi value name dan damage senjata.
+//C. Buatlah method attack() yang akan menampilkan pesan menyerang dan menunjukkan kerusakan yang diberikan oleh senjata.
+//D. Buatlah objek Weapon dengan 3 nama dan kerusakan yang berbeda dan uji method attack().
 #include <iostream>
 
 using namespace std;
@@ -12,8 +17,20 @@ class WeaponCharacter {
             name = name_weapon;
             damage = damage_weapon;
     }
+
+    void attack() {
+        cout << name << " Total Damage:  " << damage << " damage! " << endl;
+    }
 };
 
 int main() {
+    
+    WeaponCharacter WeaponCharacter_1("Pedang", 25);
+    WeaponCharacter WeaponCharacter_2("Panah", 50);
+    WeaponCharacter WeaponCharacter_3("Tombak", 10);
 
+    WeaponCharacter_1.attack();
+    WeaponCharacter_2.attack();
+    WeaponCharacter_3.attack();
+    
 }
